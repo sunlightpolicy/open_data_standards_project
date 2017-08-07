4ca13f5cad61bdc261e9a0424fe19ea13173e2d7
aycrazy <andrewyaspan@gmail.com>
2017-08-07 10:01:36 -0400

    test bldsjs 7

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -21,13 +21,13 @@ function show_data(table, mymap) {
 	var demo = L.icon({iconUrl:'https://upload.wikimedia.org/wikipedia/commons/6/67/Hammer_icon_-_Noun_Project_8246.svg', iconSize: [19, 19] });
 	var fence = L.icon({iconUrl:'https://camo.githubusercontent.com/3d0b70e5e229dc6985ee795acb6c96fa03e0d5a7/68747470733a2f2f63646e312e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f7265616c2d6573746174652d7365742d322f3531322f34302d3531322e706e67'
 		, iconSize: [19, 19] });
-	var roof = L.icon({iconUrl:'../images/roof.png', iconSize: [19, 19] });
-	var building = L.icon({iconUrl:'../images/building.png', iconSize: [19, 19] });
-	var pool = L.icon({iconUrl:'../images/pool.png', iconSize: [19, 19] });
-	var electric = L.icon({iconUrl:'../images/electric.png', iconSize: [19, 19] });
-	var grading = L.icon({iconUrl:'../images/grading.png', iconSize: [19, 19] });
-	var mechanic = L.icon({iconUrl:'../images/mechanic.png', iconSize: [19, 19] });
-	var plumbing = L.icon({iconUrl:'../images/mechanic.png', iconSize: [19, 19] });
+	var roof = L.icon({iconUrl:'images/roof.png', iconSize: [19, 19] });
+	var building = L.icon({iconUrl:'images/building.png', iconSize: [19, 19] });
+	var pool = L.icon({iconUrl:'images/pool.png', iconSize: [19, 19] });
+	var electric = L.icon({iconUrl:'images/electric.png', iconSize: [19, 19] });
+	var grading = L.icon({iconUrl:'images/grading.png', iconSize: [19, 19] });
+	var mechanic = L.icon({iconUrl:'images/mechanic.png', iconSize: [19, 19] });
+	var plumbing = L.icon({iconUrl:'images/mechanic.png', iconSize: [19, 19] });
 
 	console.log(table);
 	
