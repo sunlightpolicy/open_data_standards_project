3859b394d915722b70377ac7f710d3f709fea6b0
aycrazy <andrewyaspan@gmail.com>
2017-08-07 10:53:22 -0400

    should be correct for SD take 3

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -3,7 +3,7 @@
 ---
 
 function show_map() { 
-	var mymap = L.map('mapid', { zoomControl:false }).setView([32.8184,-116.3356],9);
+	var mymap = L.map('mapid', { zoomControl:false }).setView([32.8715,-116.7723],9);
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
 	maxZoom: 18,
 	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
