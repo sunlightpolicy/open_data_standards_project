20e2711c4cf7dafd9a4014d6cb9aa406b32ac766
aycrazy <andrewyaspan@gmail.com>
2017-08-07 09:20:10 -0400

    test bldsjs 3

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -31,9 +31,14 @@ function show_data(table, mymap) {
 
 	console.log(table);
 	
-	pt_lower = table['permitypemapped'].toLowerCase();
+	if(table['permitypemapped']){
+		pt = table['permitypemapped'].toLowerCase();
+	}
+	else{
+		pt = table['permitypemapped']
+	}
 
-	if (pt_lower === 'building'){
+	if (pt === 'building'){
 	    var marker = L.marker([table['latitude'], table['longitude']], {icon: building}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
 	  }
 	  //else {
