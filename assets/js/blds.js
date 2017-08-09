89ddd9452e1d07d082462be28faf52f804515813
aycrazy <andrewyaspan@gmail.com>
2017-08-09 10:36:31 -0400

    hacked it?3

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -110,10 +110,116 @@ function create_line_chart(permit_dict, element){
 		var myChart = new Chart(ctx, {
 			type : 'line',
 			data:{
-				labels: Object.keys(permit_dict),
+				labels: ['January','February','March','April','May','June','July','August','September',
+				'October','November','December'],
 				datasets: [{
-					label: 'Buildling Permits by type in San Diego by Month',
-					data: Object.values(permit_dict),
+					label: tallies[0][0],
+					data: tallies[0].slice(1,13),
+					backgroundColor: [
+					                'rgba(255, 99, 132, 0.2)',
+					                'rgba(54, 162, 235, 0.2)',
+					                'rgba(255, 206, 86, 0.2)',
+					                'rgba(75, 192, 192, 0.2)',
+					                'rgba(153, 102, 255, 0.2)',
+					                'rgba(255, 159, 64, 0.2)'
+					            ],
+					            borderColor: [
+					                'rgba(255,99,132,1)',
+					                'rgba(54, 162, 235, 1)',
+					                'rgba(255, 206, 86, 1)',
+					                'rgba(75, 192, 192, 1)',
+					                'rgba(153, 102, 255, 1)',
+					                'rgba(255, 159, 64, 1)'
+					            ],
+					            borderWidth: 1
+					        },
+					        {
+					label: tallies[1][0],
+					data: tallies[1].slice(1,13),
+					backgroundColor: [
+					                'rgba(255, 99, 132, 0.2)',
+					                'rgba(54, 162, 235, 0.2)',
+					                'rgba(255, 206, 86, 0.2)',
+					                'rgba(75, 192, 192, 0.2)',
+					                'rgba(153, 102, 255, 0.2)',
+					                'rgba(255, 159, 64, 0.2)'
+					            ],
+					            borderColor: [
+					                'rgba(255,99,132,1)',
+					                'rgba(54, 162, 235, 1)',
+					                'rgba(255, 206, 86, 1)',
+					                'rgba(75, 192, 192, 1)',
+					                'rgba(153, 102, 255, 1)',
+					                'rgba(255, 159, 64, 1)'
+					            ],
+					            borderWidth: 1
+					        }
+					        {
+					label: tallies[2][0],
+					data: tallies[2].slice(1,13),
+					backgroundColor: [
+					                'rgba(255, 99, 132, 0.2)',
+					                'rgba(54, 162, 235, 0.2)',
+					                'rgba(255, 206, 86, 0.2)',
+					                'rgba(75, 192, 192, 0.2)',
+					                'rgba(153, 102, 255, 0.2)',
+					                'rgba(255, 159, 64, 0.2)'
+					            ],
+					            borderColor: [
+					                'rgba(255,99,132,1)',
+					                'rgba(54, 162, 235, 1)',
+					                'rgba(255, 206, 86, 1)',
+					                'rgba(75, 192, 192, 1)',
+					                'rgba(153, 102, 255, 1)',
+					                'rgba(255, 159, 64, 1)'
+					            ],
+					            borderWidth: 1
+					        }
+					        {
+					label: tallies[3][0],
+					data: tallies[3].slice(1,13),
+					backgroundColor: [
+					                'rgba(255, 99, 132, 0.2)',
+					                'rgba(54, 162, 235, 0.2)',
+					                'rgba(255, 206, 86, 0.2)',
+					                'rgba(75, 192, 192, 0.2)',
+					                'rgba(153, 102, 255, 0.2)',
+					                'rgba(255, 159, 64, 0.2)'
+					            ],
+					            borderColor: [
+					                'rgba(255,99,132,1)',
+					                'rgba(54, 162, 235, 1)',
+					                'rgba(255, 206, 86, 1)',
+					                'rgba(75, 192, 192, 1)',
+					                'rgba(153, 102, 255, 1)',
+					                'rgba(255, 159, 64, 1)'
+					            ],
+					            borderWidth: 1
+					        }
+					        {
+					label: tallies[4][0],
+					data: tallies[4].slice(1,13),
+					backgroundColor: [
+					                'rgba(255, 99, 132, 0.2)',
+					                'rgba(54, 162, 235, 0.2)',
+					                'rgba(255, 206, 86, 0.2)',
+					                'rgba(75, 192, 192, 0.2)',
+					                'rgba(153, 102, 255, 0.2)',
+					                'rgba(255, 159, 64, 0.2)'
+					            ],
+					            borderColor: [
+					                'rgba(255,99,132,1)',
+					                'rgba(54, 162, 235, 1)',
+					                'rgba(255, 206, 86, 1)',
+					                'rgba(75, 192, 192, 1)',
+					                'rgba(153, 102, 255, 1)',
+					                'rgba(255, 159, 64, 1)'
+					            ],
+					            borderWidth: 1
+					        }
+					        {
+					label: tallies[5][0],
+					data: tallies[5].slice(1,13),
 					backgroundColor: [
 					                'rgba(255, 99, 132, 0.2)',
 					                'rgba(54, 162, 235, 0.2)',
@@ -169,15 +275,10 @@ permit_dict =  {
 
 {% endfor %}
 
-{% for line in  site.data.sd_tallies %}
-	
-	var line = {{ line | jsonify}}
-	
-{% endfor %}
 
-var test = {{ site.data.sd_tallies | jsonify}}
+var tallies = {{ site.data.sd_tallies | jsonify}}
 
-console.log(test)
+console.log(tallies)
 
-create_line_chart(permit_dict,'myChart');
+create_line_chart(tallies,'myChart');
 
