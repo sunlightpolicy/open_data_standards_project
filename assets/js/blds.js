60786d7cae51e20164bf332750421a8d4a37e6c4
aycrazy <andrewyaspan@gmail.com>
2017-08-11 11:33:35 -0400

    functionify map fix

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -101,13 +101,6 @@ function update_dict(permit_dict,pt){
 	return permit_dict;
 }
 
-data_info = tallies.map(function(i){
-  return {label: i[0],
-            data : Object.values(i).slice(1,13),
-            backgroundColor : 'rgba(255,255,255,0.2)',
-            borderColor : colors[i],
-            borderWidth: 1}
-                                });
 
 function create_line_chart(tallies, element){
 		var ctx = document.getElementById(element);
@@ -207,5 +200,13 @@ var tallies = {{ site.data.sd_tallies | jsonify}}
 
 console.log(tallies)
 
-create_line_chart(tallies,'myChart');
+data_info = tallies.map(function(i){
+  return {label: i[0],
+            data : Object.values(i).slice(1,13),
+            backgroundColor : 'rgba(255,255,255,0.2)',
+            borderColor : colors[i],
+            borderWidth: 1}
+                                });
+
+create_line_chart(data_info,'myChart');
 
