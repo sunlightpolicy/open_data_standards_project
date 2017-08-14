bd929d5586cae962949b4fffc6c42e611beadfe0
aycrazy <andrewyaspan@gmail.com>
2017-08-14 14:56:11 -0400

    cleaning code 14 and most modulated -- make header files

--- assets/js/blds.js
+++ assets/js/blds.js
@@ -60,19 +60,6 @@ function plot_icon(pt, row, mymap){
 
 function show_data(table, mymap) {
 
-	var demo = L.icon({iconUrl:'https://upload.wikimedia.org/wikipedia/commons/6/67/Hammer_icon_-_Noun_Project_8246.svg', iconSize: [19, 19] });
-	var fence = L.icon({iconUrl:'https://camo.githubusercontent.com/3d0b70e5e229dc6985ee795acb6c96fa03e0d5a7/68747470733a2f2f63646e312e69636f6e66696e6465722e636f6d2f646174612f69636f6e732f7265616c2d6573746174652d7365742d322f3531322f34302d3531322e706e67'
-		, iconSize: [19, 19] });
-	var roof = L.icon({iconUrl:'/open_data_standards_project/images/roof.png', iconSize: [19, 19] });
-	var building = L.icon({iconUrl:'/open_data_standards_project/images/building.png', iconSize: [19, 19] });
-	var pool = L.icon({iconUrl:'/open_data_standards_project/images/pool.png', iconSize: [19, 19] });
-	var electric = L.icon({iconUrl:'/open_data_standards_project/images/electric.png', iconSize: [19, 19] });
-	var grading = L.icon({iconUrl:'/open_data_standards_project/images/grading.png', iconSize: [19, 19] });
-	var mechanic = L.icon({iconUrl:'/open_data_standards_project/images/mechanic.png', iconSize: [19, 19] });
-	var plumbing = L.icon({iconUrl:'/open_data_standards_project/images/mechanic.png', iconSize: [19, 19] });
-
-	//console.log(table);
-	
 	if(table['permittypemapped'] != null){
 		pt = table['permittypemapped'].toLowerCase();
 	}
@@ -80,58 +67,8 @@ function show_data(table, mymap) {
 		pt = table['permittypemapped']
 	};
 
-	//console.log( pt, table['latitude'], table['longitude']);
-
 	plot_icon(pt,table, mymap);
-	// if (pt === 'building'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: building}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'roofing'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: roof}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//  }
- //  	else if (pt === 'demolition'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: demo}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'fencing'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: fence}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'pool' || pt == 'spa'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: pool}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'mechanical'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: mechanic}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'electrical'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: electric}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else if (pt === 'grading'){
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: grading}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	// else {
-	// 	if(table['latitude'] != null){
-	//     var marker = L.marker([table['latitude'], table['longitude']], {icon: plumbing}).bindPopup( '<p>'+ table['description']+'</p>'+'<p>'+table['issueddate']+ '</p>').addTo(mymap);
-	//   	}
-	//   }
-	  //else {
-	  //  var marker = L.marker([table['latitude'], table['longitude']], {icon: kcIcon}).bindPopup( '<p>'+ table['business_dba']+'</p>'+'<p>'+table['business_type']+ '</p>').addTo(mymap);
-	    //}
-	  //}
+	
 	return pt;
 }
 
