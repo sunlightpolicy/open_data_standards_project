69439148cb10175f81388d3879fdf9c2d7ff2f89
aycrazy <andrewyaspan@gmail.com>
2017-08-15 13:53:16 -0400

    scatterplot in pynb for procurements!

--- scripts/python/standardize.py
+++ scripts/python/standardize.py
@@ -26,9 +26,9 @@ def scrape_datasets(main_url):
 
 	return [[x['href'].strip(url_pre) for x in bs4.BeautifulSoup(make_html(pm,results),'html.parser').find_all('a','browse2-result-name-link') if x['href']] for results in all_results]
 
-def print_nums(df):
-    for name in proc_data.city.unique():
-        print(name+': '+str(len(proc_data[proc_data.city==name])))
+def print_nums(df, name_col):
+    for name in df[name_col].unique():
+        print(name+': '+str(len(df[df[name_col]==name])))
         
 
 def make_one_column(datasets, name):
