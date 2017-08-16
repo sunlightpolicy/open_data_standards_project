f4a9728469de3faaba4651df3bdc31de614729ab
aycrazy <andrewyaspan@gmail.com>
2017-08-16 14:38:03 -0400

    procurement test 26 test***

--- assets/js/procurement.js
+++ assets/js/procurement.js
@@ -35,11 +35,9 @@ function unique_vals(procurement){
 	unique_list = [];
 	procurement.map(function(row){
 		console.log(row['categoryMonth']);
-		if(!(row['categoryMonth'] in unique_list)){
-			unique_list.push(row['categoryMonth']);
-		}});
+		unique_list.push(row['categoryMonth']);
 
-	return unique_list;
+	return Array.from(new Set(unique_list));
 }
 
 
